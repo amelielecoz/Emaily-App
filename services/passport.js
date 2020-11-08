19f2cff62e9d220dc4fe6cbfbabc7f618ecec2a3
Amelie Le Coz <lecozamelie@hotmail.com>
2020-11-08 15:04:55 +0100

    add Serialize/Deserialize passport

--- services/passport.js
+++ services/passport.js
@@ -3,14 +3,37 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
 
-
 const User = mongoose.model('users')
+
+//Put user ID into a cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+//Receive the ID stored in the cookie and retrieve corresponding user
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+})
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    const user = new User({ googleId: profile.id});
-    user.save();
+}, (accesToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id})
+    .then( existingUser => {
+        if (existingUser) {
+            done(null, existingUser)
+        } else {
+            new User({ googleId: profile.id})
+                .save()
+                .then( user => {
+                    done(null, user);
+                });
+        }
+    })
+    
 }));
