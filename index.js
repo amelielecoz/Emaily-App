36b1754052f2915a58fccd1f522af06ab27077bb
Amelie Le Coz <lecozamelie@hotmail.com>
2020-11-08 15:20:59 +0100

    add cookie session

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 const mongoose = require('mongoose');
@@ -9,7 +11,19 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //lasts 30 days
+    keys: [keys.cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
+
+app.get('/api/user', (req, res) => {
+    res.send(req.user)
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
