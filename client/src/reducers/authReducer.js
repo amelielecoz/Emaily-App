ba71492f5c31e0057cfddadd9eb267ba76b531c6
Amelie Le Coz <lecozamelie@hotmail.com>
2020-11-10 10:17:30 +0100

    AuthReducer returning values

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,11 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+//returns null by default
+export default function(state = null, action) {
+  console.log(action);
     switch (action.type) {
+      case FETCH_USER:
+        return action.payload || false //returns the user model or false if action.payload === '' (empty string)
       default:
         return state;
     }
