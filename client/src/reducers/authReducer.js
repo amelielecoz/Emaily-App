6a4c2e09649a3653c6239e555b2269e6aa1c734d
Amelie Le Coz <lecozamelie@hotmail.com>
2020-11-10 10:35:14 +0100

    Accessing State in the Header

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,7 +2,6 @@ import { FETCH_USER } from '../actions/types';
 
 //returns null by default
 export default function(state = null, action) {
-  console.log(action);
     switch (action.type) {
       case FETCH_USER:
         return action.payload || false //returns the user model or false if action.payload === '' (empty string)
