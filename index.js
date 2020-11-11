499abe19dc989cc4200fb43467cb002ab192c127
Amelie Le Coz <lecozamelie@hotmail.com>
2020-11-11 15:21:13 +0100

    Preparing for deployment on Heroku

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('./models/User');
 require('./services/passport');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { dirname } = require('path');
 
 
 mongoose.connect(keys.mongoURI);
@@ -23,6 +24,19 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file or main.css file 
+    app.use(express.static('client/build'));
+
+    //Express will serve
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
