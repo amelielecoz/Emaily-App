bf55dbb8ee56e1e3e48fa6378faaa1cb3a8c650e
Amelie Le Coz <lecozamelie@hotmail.com>
2020-11-11 21:25:49 +0100

    Update Recipient.js

--- models/Recipient.js
+++ models/Recipient.js
@@ -3,7 +3,9 @@ const { Schema } = mongoose;
 
 const recipientSchema = new Schema({
     email: String,
-    responded: { type: Boolean, default: false },
+    responded: { type: Boolean, default: false }
 });
+
+module.exports = recipientSchema;
 
 mongoose.model('recipients', recipientSchema);
