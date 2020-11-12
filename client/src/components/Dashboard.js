ed8a7a29aadac75e7a2d0271ca938bba640c12ca
Amelie Le Coz <lecozamelie@hotmail.com>
2020-11-12 13:54:49 +0100

    Adding Materliaze Icons

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,6 +4,12 @@ const Dashboard = () => {
     return (
         <div>
             Dashboard
+            <div className="fixed-action-btn">
+                <a className="btn-floating btn-large red">
+                    <i className="material-icons">add</i>
+                </a>
+
+            </div>
         </div>
     )
 }
